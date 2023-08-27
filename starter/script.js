7fd32466de24090329a8a6ec456bfbc3a817ace4
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-08-27 17:57:12 +0530

    add

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,17 @@
 "use strict";
 
+//todo: Selecting Elements
+
 const score0Element = document.querySelector("#score--0");
 const score1Element = document.getElementById("score--1");
+const diceImg = document.querySelector(".dice");
 
 //? .getElementById("IdName"):
 //* this method is used to select the id mentioned inside it.
 //* The only difference between .querySelector("") & .getElementById("") is that querySelector is universal (i.e., it can select by elementName, .className, #idName) but getElementById only selects the Id inside it without # symbol.
+
+//todo: PHASE 1: Starting Conditions
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceImg.classList.add("hidden");
