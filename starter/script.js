ab75c1de20894b0132673b6458413cf5bd716056
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-08-27 17:44:45 +0530

    add

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,8 @@
 "use strict";
 
-const score0 = document.querySelector("#score--0");
+const score0Element = document.querySelector("#score--0");
+const score1Element = document.getElementById("score--1");
+
+//? .getElementById("IdName"):
+//* this method is used to select the id mentioned inside it.
+//* The only difference between .querySelector("") & .getElementById("") is that querySelector is universal (i.e., it can select by elementName, .className, #idName) but getElementById only selects the Id inside it without # symbol.
